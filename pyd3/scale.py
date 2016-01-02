806d067c1ff754153285abed0552fd0ab6e1286c
Nicolas P. Rougier <Nicolas.Rougier@inria.fr>
2016-01-02 10:49:20 +0100

    [WIP] Added linear scale

---
+++ pyd3/scale.py
@@ -0,0 +1,318 @@
+# -----------------------------------------------------------------------------
+# Copyright (c) 2016, Nicolas P. Rougier. All rights reserved.
+# Distributed under the terms of the new BSD License.
+# -----------------------------------------------------------------------------
+"""
+This is a python translation of the `d3-scale
+<https://github.com/d3/d3-scale>`_ javascript module.
+
+Scales are a convenient abstraction for a fundamental task in
+visualization: mapping a dimension of abstract data to a visual
+representation. Although most often used for position-encoding quantitative
+data, such as mapping a measurement in meters to a position in pixels for dots
+in a scatterplot, scales can represent virtually any visual encoding, such as
+diverging colors, stroke widths, or symbol size. Scales can also be used with
+virtually any type of data, such as named categorical data or discrete data
+that requires sensible breaks.
+
+For continuous quantitative data, you typically want a linear scale. (For time
+series data, a time scale.) If the distribution calls for it, consider
+transforming data using a power or log scale. A quantize scale may aid
+differentiation by rounding continuous data to a fixed set of discrete values;
+similarly, a quantile scale computes quantiles from a sample population, and a
+threshold scale allows you to specify arbitrary breaks in continuous
+data. Several built-in sequential color scales are also provided. (If you don’t
+like these palettes, try ColorBrewer.)
+
+For discrete ordinal (ordered) or categorical (unordered) data, an ordinal
+scale specifies an explicit mapping from a set of data values to a
+corresponding set of visual attributes (such as colors). The related band and
+point scales are useful for position-encoding ordinal data, such as bars in a
+bar chart or dots in an categorical scatterplot. Several built-in categorical
+color scales are also provided.
+
+Scales have no intrinsic visual representation. However, most scales can
+generate and format ticks for reference marks to aid in the construction of
+axes.
+"""
+
+import numpy as np
+from pyd3 import interpolate
+
+py_range = range
+
+def interpolate_number(x, xp, yp, clamp=True):
+    """
+    Specialized interpolation for array of scalars
+    """
+    x = np.asarray(x)
+
+    # Extrapolate
+    if not clamp:
+        # Single value
+        if len(x.shape) == 0:
+            if   x < xp[0]:  return yp[ 0] + (x-xp[ 0])*(yp[ 0]-yp[ 1]) / (xp[ 0]-xp[ 1])
+            elif x > xp[-1]: return yp[-1] + (x-xp[-1])*(yp[-1]-yp[-2]) / (xp[-1]-xp[-2])
+            else:            return np.interp(x, xp, yp)
+        # Values list
+        else:
+            y = np.interp(x, xp, yp)
+            y[x < xp[ 0]] = yp[ 0] + (x[x<xp[ 0]]-xp[ 0]) * (yp[ 0]-yp[ 1]) / (xp[ 0]-xp[ 1])
+            y[x > xp[-1]] = yp[-1] + (x[x>xp[-1]]-xp[-1]) * (yp[-1]-yp[-2]) / (xp[-1]-xp[-2])
+            return y
+
+    # Interpolate
+    return np.interp(x, xp, yp)
+
+
+def interpolate_time(x, xp, yp, clamp=True):
+    """
+    Specialized interpolation for array of datetime values
+    """
+
+    x = np.asarray(x)
+    delta = np.cumsum(yp[1:] - yp[:-1])
+    delta = np.insert(delta, 0, 0)
+    dtype = delta.dtype
+
+    interpolated = np.array(interpolate_number(x, xp, delta.astype(int), clamp))
+    return yp[0] + interpolated.astype(delta.dtype)
+
+
+def interpolate_value(x, xp, yp, clamp=True):    
+    """
+    Generic interpolation
+    """
+
+    x = np.asarray(x)
+
+    # Range may have been given as ["1", "2"]
+    #if not isinstance(xp, np.ndarray):
+    #    try:    xp = [float(v) for v in xp]
+    #    except: pass
+    #xp = np.asarray(xp)
+        
+    n = len(xp)
+    
+    # Build (n-1) interpolators for each interval in yp
+    interpolators = []
+    for i in range(len(yp)-1):
+        interpolators.append(interpolate.value(yp[i],yp[i+1]))
+
+    # Find corresponding interpolator foreach x value
+    xi = np.searchsorted(xp,x)
+    
+    # Single value
+    if len(x.shape) == 0:
+        # Find indices of x within xp
+        if xi == 0:
+            xi = 1   # index 0 (= prepend) is invalid
+        elif xi == n:
+            xi = n-1 # index n (= append) is invalid
+        xi -= 1
+
+        # Normalized x values in each interval
+        v = np.arange(len(xp))
+        nx = interpolate_number(x, xp, np.arange(len(xp)), clamp=clamp) - xi
+        return interpolators[xi](nx)
+    
+    # Values list
+    else:
+        # Find indices of x within xp
+        xi[xi==0] = 1   # index 0 (= prepend) is invalid
+        xi[xi==n] = n-1 # index n (= append) is invalid
+        xi -= 1
+
+        # Normalized x values in each interval
+        v = np.arange(len(xp))
+        nx = interpolate_number(x, xp, np.arange(len(xp)), clamp=clamp) - xi
+
+        # Get output value for each x
+        return [interpolators[i](x) for i,x in zip(xi,nx)]
+
+
+
+class ContinuousScale(object):
+    """
+    Continuous scales map a continuous, quantitative input domain to a
+    continuous output range. If the range is also numeric, the mapping may be
+    inverted. A continuous scale is not constructed directly; instead, try a
+    linear, power, log, identity, time or sequential color scale.
+
+    # continuous(value)
+
+    Given a value from the domain, returns the corresponding value from the
+    range. If the given value is outside the domain, and clamping is not
+    enabled, the mapping may be extrapolated such that the returned value is
+    outside the range. For example, to apply a position encoding::
+
+    x = scale.linear(domain=[10, 130], range=[0, 960])
+    x(20) # 80
+    x(50) # 320
+
+    Or to apply a color encoding::
+
+    color = scale.linear(domain=[10, 100], range=["brown", "steelblue"])
+    color(20) # "#9a3439"
+    color(50) # "#7b5167"
+    """
+    
+    def __init__(self, domain=[0,1], range=[0,1], clamp=False):
+
+        self._update_domain_range(domain, range)
+        self._clamp  = clamp
+        if isinstance(range[0], (int,float)):
+            self._interpolate = interpolate_number
+        elif isinstance(range[0], np.datetime64):
+            self._interpolate = interpolate_time
+        else:
+            self._interpolate = interpolate_value
+
+    @property
+    def clamp(self):
+        return self._clamp
+
+    @clamp.setter
+    def clamp(self, clamp):
+        self._clamp = clamp
+
+    @property
+    def domain(self):
+        return self._domain
+
+    @domain.setter
+    def domain(self, domain):
+        self.update_domain_range(domain, self._range)
+
+    @property
+    def range(self):
+        return self._range
+
+    @range.setter
+    def range(self, range):
+        self._update_domain_range(self._domain, range)
+
+
+    def _update_domain_range(self, domain, range):
+
+        self._domain = domain
+        self._range = range
+        
+        # Ensure len(domain) == len(range)
+        n = min(len(self.domain), len(self.range))
+        domain = domain[:n]
+        range  = range[:n]
+
+        # Coerce domain values if necessay
+        # (domain may have been given as ["1", "2"])
+        if not isinstance(domain, np.ndarray):
+            try:
+                domain = [float(v) for v in domain]
+            except:
+                pass
+
+        # Forward domain & range
+        # (domain must be sorted in increasing order)
+        domain = np.asarray(domain)
+        sorted = np.argsort(domain)
+        self._forward_domain = domain[sorted]
+        if isinstance(range, np.ndarray):
+            self._forward_range = range[sorted]
+        else:
+            self._forward_range = [range[i] for i in sorted]
+
+        # Try to convert range to a numpy array if possible
+        try:
+            self._forward_range = np.asarray(self._forward_range)
+        except:
+            pass
+            
+        # Inverse domain & range
+        # (range must be sorted in increasing order)
+        if not isinstance(range, np.ndarray):
+            try:
+                range = [float(v) for v in range]
+            except:
+                self._inverse_domain = None
+                self._inverse_range = None
+                return
+        range = np.asarray(range)
+        sorted = np.argsort(range)        
+        self._inverse_range = range[sorted]
+        self._inverse_domain = domain[sorted]
+            
+
+
+    
+class LinearScale(ContinuousScale):
+    def __init__(self, domain=[0,1], range=[0,1], clamp=False):
+        ContinuousScale.__init__(self, domain, range, clamp)
+
+    def __call__(self, values):
+        return self._interpolate(values, self._forward_domain, self._forward_range, self._clamp)
+
+    def invert(self, values):
+        if self._inverse_range is not None:
+            return self._interpolate(values, self._inverse_range, self._inverse_domain, self._clamp)
+        else:
+            return None
+
+
+linear = LinearScale
+    
+
+
+# class QuantizeScale(object):
+#     def __init__(self, domain=[0,1], range=[0,1], clamp=True):
+#         self._domain = domain
+#         self._range  = np.linspace(0,len(range)-1,num=len(domain))
+#         self._values = list(range)
+#         self._clamp  = clamp
+#         self._interpolate = interpolate
+        
+#     def __call__(self, values):
+#         indices = np.round(self._interpolate(values, self._domain, self._range))
+#         return self._values[indices.astype(int)]
+
+#     def invert(self, values):
+#         values = np.asarray(values)
+#         if len(values.shape) == 0:
+#             index = self._values.index(values)
+#             return self._interpolate(index, self._range, self._domain)
+#         else:
+#             indices = [self._values.index(value) for value in values]
+#             return self._interpolate(indices, self._range, self._domain)
+
+
+# x = QuantizeScale(domain=[10,100], range=[1,2,4])
+# print(x(20))
+# print(x(50))
+# print(x(80))
+# print(x.invert(4))
+# print(x.invert([1,2,4]))
+
+# x = LinearScale(domain=[0,100], range=[0,1])
+# print(x.invert(x(np.linspace(0,100,11))))
+#x = LinearScale(domain=[0,100], range=[Color("black"),Color("white")])
+#print(x(100))
+#print(x.invert(x(np.linspace(0,100,11))))
+
+# def interpolate_time(x, xp, yp):
+#     delta = np.cumsum(T[1:] - T[:-1])
+#     delta = np.insert(delta, 0, 0)
+#     dtype = delta.dtype
+#     return yp[0] + np.interp(x, xp, delta.astype(int)).astype(delta.dtype)
+
+
+# T = np.array(['2005-01-01T00:00', '2005-01-02T00:00', '2005-01-03T00:00'], dtype='datetime64')
+# print(interpolate_time( [0], [0,1,2], T)[0])
+# print(interpolate_time( [1], [0,1,2], T)[0])
+# print(interpolate_time( [2], [0,1,2], T)[0])
+# print(interpolate_time( [-1], [0,1,2], T, False)[0])
+
+# print(interpolate( -1, [0,1], [0,1], clamp=True))
+# print(interpolate( +2, [0,1], [0,1], clamp=True))
+# print(interpolate( -1, [0,1], [0,1], clamp=False))
+# print(interpolate( +2, [0,1], [0,1], clamp=False))
+
+
---
+++ pyd3/tests/test_scale_linear.py
@@ -0,0 +1,160 @@
+# -----------------------------------------------------------------------------
+# Copyright (c) 2016, Nicolas P. Rougier. All rights reserved.
+# Distributed under the terms of the new BSD License.
+# -----------------------------------------------------------------------------
+import unittest
+import numpy as np
+from pyd3 import scale
+
+class test_scale_linear(unittest.TestCase):
+
+    def test_1(self):
+        """
+        linear() has the expected defaults
+        """
+        s = scale.linear()
+        self.assertEqual(s.domain, [0,1])
+        self.assertEqual(s.range, [0,1])
+        self.assertEqual(s.clamp, False)
+
+    def test_2(self):
+        """
+        linear(x) maps a domain value x to a range value y"
+        """
+
+        s = scale.linear(range=[1,2])
+        self.assertEqual(s(0.5), 1.5)
+
+    def test_3(self):
+        """
+        linear(x) ignores extra range values if the domain is smaller than the range
+        """
+        s = scale.linear(domain=[-10,0], range=["red", "white", "green"], clamp=True)
+        self.assertEqual(s(-5), "#ff8080")
+        self.assertEqual(s(50), "#ffffff")
+
+    def test_4(self):
+        """
+        linear(x) ignores extra domain values if the range is smaller than the domain
+        """
+        s = scale.linear(domain=[-10,0,100], range=["red", "white"], clamp=True)
+        self.assertEqual(s(-5), "#ff8080")
+        self.assertEqual(s(50), "#ffffff")
+
+    def test_5(self):
+        """
+        linear(x) maps an empty domain to the range start
+        """
+        s = scale.linear(domain=[0,0], range=[1,2])
+        self.assertEqual(s(0), 1)
+        
+        s = scale.linear(domain=[0,0], range=[2,1])
+        self.assertEqual(s(1), 2)
+    
+    def test_6(self):
+        """
+        linear(x) can map a bilinear domain with two values to the corresponding
+        range
+        """
+        s = scale.linear(domain=[1, 2])
+        self.assertEqual(s.domain, [1,2])
+        self.assertEqual(s(0.5), -0.5)
+        self.assertEqual(s(1.0),  0.0)
+        self.assertEqual(s(1.5),  0.5)
+        self.assertEqual(s(2.0),  1.0)
+        self.assertEqual(s(2.5),  1.5)
+        self.assertEqual(s.invert(-0.5), 0.5)
+        self.assertEqual(s.invert( 0.0), 1.0)
+        self.assertEqual(s.invert( 0.5), 1.5)
+        self.assertEqual(s.invert( 1.0), 2.0)
+        self.assertEqual(s.invert( 1.5), 2.5)
+
+    def test_7(self):
+        """
+        linear(x) can map a polylinear domain with more than two values to the
+        corresponding range
+        """
+        s = scale.linear(domain=[-10, 0, 100], range=["red", "white", "green"])
+        self.assertEqual(s.domain, [-10, 0, 100])
+        self.assertEqual(s(-5), "#ff8080")
+        self.assertEqual(s(50), "#80c080")
+        self.assertEqual(s(75), "#40a040")
+  
+        s = scale.linear(domain=[4, 2, 1], range=[1, 2, 4])
+        self.assertEqual(s(1.5), 3)
+        self.assertEqual(s(3), 1.5)
+        self.assertEqual(s.invert(1.5), 3)
+        self.assertEqual(s.invert(3), 1.5)
+        
+        s = scale.linear(domain=[1, 2, 4],range=[4, 2, 1])
+        self.assertEqual(s(1.5), 3)
+        self.assertEqual(s(3), 1.5)
+        self.assertEqual(s.invert(1.5), 3)
+        self.assertEqual(s.invert(3), 1.5)
+
+    def test_8(self):
+        """
+        linear.invert(y) maps a range value y to a domain value x
+        """
+        s = scale.linear(range=[1,2])
+        self.assertEqual(s.invert(1.5), .5)
+
+    def test_9(self):
+        """
+        linear.invert(y) maps an empty range to the domain start
+        """
+        s = scale.linear(domain=[1,2], range=[0,0])
+        self.assertEqual(s.invert(0), 1)
+        s = scale.linear(domain=[2,1], range=[0,0])
+        self.assertEqual(s.invert(1), 2)
+
+    def test_10(self):
+        """
+        linear.invert(y) coerces range values to numbers
+        """
+        s = scale.linear(range=["0", "2"])
+        self.assertEqual(s.invert(1), .5)
+
+        s = scale.linear(range=[np.datetime64("1990-01-01"), np.datetime64("1991-01-01")])
+        self.assertEqual(s(.5), np.datetime64("1990-07-02"))
+
+    def test_11(self):
+        """
+        linear.invert(y) returns None if the range is not coercible to number
+        """
+        s = scale.linear(range=["#000", "#fff"])
+        self.assertEqual(s.invert("#999"), None)
+        s = scale.linear(range=[0, "#fff"])
+        self.assertEqual(s.invert("#999"), None)
+
+    def test_12(self):
+        """
+        linear.domain(domain) accepts an array of numbers
+        """
+        self.assertEqual( scale.linear(domain=[]).domain, [])
+        self.assertEqual( scale.linear(domain=[1,0]).domain, [1,0])
+        self.assertEqual( scale.linear(domain=[1,2,3]).domain, [1,2,3])
+
+    def test_16(self):
+        """
+        linear.range(range) does not coerce range to numbers
+        """
+        s = scale.linear(range=["0px", "2px"])
+        self.assertEqual(s.range, ["0px", "2px"])
+        self.assertEqual(s(.5), "1px")
+
+    def test_17(self):
+        """
+        linear.range(range) can accept range values as colors
+        """
+        s = scale.linear(range=["red", "blue"])
+        self.assertEqual(s(.5), "#800080")
+
+        s = scale.linear(range=["#ff0000", "#0000ff"])
+        self.assertEqual(s(.5), "#800080")
+
+        s = scale.linear(range=["#f00", "#00f"])
+        self.assertEqual(s(.5), "#800080")
+
+if __name__ == "__main__":
+    unittest.main()
